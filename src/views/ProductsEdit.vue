76c7e6390055f7c22dcf343a03a7506e22b347c7
Jhon Erick Domingues <jhon.erick@processmind.com.br>
2019-09-06 11:18:40 -0300

    ajustes visuais

--- src/views/ProductsEdit.vue
+++ src/views/ProductsEdit.vue
@@ -3,92 +3,102 @@
     <b-container>
       <b-row>
         <b-col>
-          <h3>New product</h3>
+          <h3>Edit product</h3>
         </b-col>
       </b-row>
-      <b-row>
-        <b-form @submit.prevent="editProduct">
-          <b-form-group
-            id="sku"
-            label="Product SKU:"
-            label-for="sku"
-            description=""
-          >
-            <b-row>
-              <b-col class="col-9">
-                <b-form-input
-                  id="sku"
-                  v-model="form.sku"
-                  v-validate="'required|min:4'"
-                  type="text"
-                  required
-                  placeholder="Enter product SKU"
-                  name="sku"
-                />
-              </b-col>
-
-              <b-col class="col-3">
-                <b-button variant="danger">
-                  123
-                </b-button>
-              </b-col>
-            </b-row>
-          </b-form-group>
-          <b-form-group
-            id="name"
-            label="Product name:"
-            label-for="name"
-            description=""
-          >
-            <b-form-input
-              id="name"
-              v-model="form.name"
-              v-validate="'required'"
-              type="text"
-              required
-              name="name"
-              placeholder="Enter product name"
-            />
-          </b-form-group>
-          <b-form-group
-            id="description"
-            label="Description name:"
-            label-for="description"
-            description=""
-          >
-            <b-form-textarea
-              id="description"
-              v-model="form.description"
-              v-validate="'required'"
-              type="text"
-              required
-              name="description"
-              placeholder="Enter product description"
-            />
-          </b-form-group>
-          <b-form-group
-            id="image"
-            label="Image URL:"
-            label-for="image"
-            description=""
-          >
-            <b-form-input
-              id="image"
-              v-model="form.image"
-              type="text"
-              required
-              placeholder="Enter product image url"
-            />
-          </b-form-group>
-          <b-form-group>
-            <b-button
-              variant="primary"
-              type="submit"
+      <b-row class="justify-content-center">
+        <b-col cols="4">
+          <b-form @submit.prevent="editProduct">
+            <b-form-group
+              id="sku"
+              label="Product SKU:"
+              label-for="sku"
+              description=""
             >
-              Save
-            </b-button>
-          </b-form-group>
-        </b-form>
+              <b-container>
+                <b-row>
+                  <b-col
+                    cols="10"
+                    class="p-0"
+                  >
+                    <b-form-input
+                      id="sku"
+                      v-model="form.sku"
+                      v-validate="'required|min:4'"
+                      type="text"
+                      required
+                      placeholder="Enter product SKU"
+                      name="sku"
+                    />
+                  </b-col>
+
+                  <b-col
+                    cols="2"
+                    class="p-0"
+                  >
+                    <b-button variant="danger">
+                      123
+                    </b-button>
+                  </b-col>
+                </b-row>
+              </b-container>
+            </b-form-group>
+            <b-form-group
+              id="name"
+              label="Product name:"
+              label-for="name"
+              description=""
+            >
+              <b-form-input
+                id="name"
+                v-model="form.name"
+                v-validate="'required'"
+                type="text"
+                required
+                name="name"
+                placeholder="Enter product name"
+              />
+            </b-form-group>
+            <b-form-group
+              id="description"
+              label="Description name:"
+              label-for="description"
+              description=""
+            >
+              <b-form-textarea
+                id="description"
+                v-model="form.description"
+                v-validate="'required'"
+                type="text"
+                required
+                name="description"
+                placeholder="Enter product description"
+              />
+            </b-form-group>
+            <b-form-group
+              id="image"
+              label="Image URL:"
+              label-for="image"
+              description=""
+            >
+              <b-form-input
+                id="image"
+                v-model="form.image"
+                type="text"
+                required
+                placeholder="Enter product image url"
+              />
+            </b-form-group>
+            <b-form-group>
+              <b-button
+                variant="primary"
+                type="submit"
+              >
+                Save
+              </b-button>
+            </b-form-group>
+          </b-form>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -123,7 +133,3 @@
         }
     };
 </script>
-
-<style scoped>
-
-</style>
